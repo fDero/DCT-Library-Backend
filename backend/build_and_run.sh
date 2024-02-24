3551e7a1c11a71eabb7e7bc13f98e82c446919c0
chrscmpl <96392458+chrscmpl@users.noreply.github.com>
2024-02-24 23:29:41 +0100

    Replace backend makefile with a bash script

---
+++ backend/build_and_run.sh
@@ -0,0 +1,24 @@
+PG="-lpq -lssl -lcrypto -pthread"
+GTEST="-lgtest -lgtest_main"
+JSN="-lcjson"
+HTTP="-lhttp_parser"
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
+
+SRC=$(find src -name "*.c")
+
+if [ "$1" = "release" ]; then
+    gcc main.c ${SRC} -o /bin/server ${PG} ${GTEST} ${JSN} ${HTTP} -I./include
+elif [ "$1" = "test" ]; then
+    TESTS=$(find tests -name "*.c")
+    if [ "$2" = "--nodb" ]; then
+        TESTS=$(IFS=' '; echo "$TESTS" | grep -v "database_connectivity")
+        SRC=$(IFS=' '; echo "$SRC" | grep -v "database_connectivity")
+        PG=""
+    fi
+    g++ ${TESTS} ${SRC} -o /bin/server ${PG} ${GTEST} ${JSN} ${HTTP} -I./include -fsanitize=address
+else
+  echo "Usage: build_and_run.sh release|test [nodb]"
+  exit 1
+fi
+
+/bin/server
