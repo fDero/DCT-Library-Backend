6081deb340edc0740cf55eddd47d113d793a87fb
fDero <francescodero@outlook.it>
2024-02-24 12:42:45 +0100

    code decluttering in http_request_decode

--- backend/src/http_requests.c
+++ backend/src/http_requests.c
@@ -59,9 +59,8 @@ http_request_t* http_request_decode(char* http_request_str){
     skip_string_terminating_with_target(request->source, &current_char_index, len, ' ', &correct);
     request->version = request->source + current_char_index;
     skip_string_terminating_with_target(request->source, &current_char_index, len, '\r', &correct);
-    if (correct){
-        correct &= (request->source[current_char_index++] == '\n');
-    }
+    correct &= (request->source[current_char_index++] == '\n');
+    
 
 
     /* HEADERS */ {
@@ -79,11 +78,9 @@ http_request_t* http_request_decode(char* http_request_str){
             skip_string_terminating_with_target_safe(request->source, &current_char_index, len, ':', ":\r\n", &correct);
             skip_character_safe(request->source, &current_char_index, len, ' ', "\r\n", &correct);
             header_values[request->headers_num++] = request->source + current_char_index;
+            
             advance_to_next_carriage_return(request->source, &current_char_index);
-            correct &= (request->source[current_char_index] == '\r');
-            if(correct){
-                request->source[current_char_index++] = '\0';
-            }
+            skip_string_terminating_with_target(request->source, &current_char_index, len, '\r', &correct);
             correct &= (request->source[current_char_index++] == '\n');
         } 
 
