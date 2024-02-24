20ff30879ecd8c84e77b0a08c558d1490a757cc3
fDero <francescodero@outlook.it>
2024-02-24 12:39:37 +0100

    code decluttering in http_request_decode

--- backend/src/http_requests.c
+++ backend/src/http_requests.c
@@ -3,6 +3,43 @@
 #include "utils.h"
 #include <stdio.h>
 
+
+void skip_string_terminating_with_target(char* string, int* current_char_index, int len, char target, bool* correct){
+    if (!*correct) return;
+    advance_to_next_target(string, current_char_index, target);
+    *correct &= (string[*current_char_index] == target);
+    *correct &= (*current_char_index < len);
+    if (*correct) {
+        string[*current_char_index] = '\0';
+    }
+    (*current_char_index)++;
+}
+
+void skip_string_terminating_with_target_safe(char* string, int* current_char_index, int len, char target, const char* targets, bool* correct){
+    if (!*correct) return;
+    advance_to_next_targets(string, current_char_index, targets);
+    *correct &= (string[*current_char_index] == target);
+    *correct &= (*current_char_index < len);
+    if (*correct) {
+        string[*current_char_index] = '\0';
+    }
+    (*current_char_index)++;
+}
+
+void skip_character_safe(char* string, int* current_char_index, int len, char target, const char* avoid, bool* correct){
+    if (!*correct) return; 
+    *correct &= (string[*current_char_index] == target);
+    (*current_char_index)++;
+    while(*current_char_index < len && string[*current_char_index] ==  target){
+        (*current_char_index)++;
+    }
+    for (int i = 0; i < strlen(avoid); i++){
+        *correct &= (string[*current_char_index] != avoid[i]);
+    }
+    *correct &= (*current_char_index < len);
+}
+
+
 http_request_t* http_request_decode(char* http_request_str){
     
     // INIT
@@ -15,100 +52,57 @@ http_request_t* http_request_decode(char* http_request_str){
     char* header_values[MAX_HEADERS];
     request->headers_num = 0;
     
-    
-    // METHOD
+    // HEADLINE
     request->method = request->source + current_char_index;
-    advance_to_next_whitespace(request->source, &current_char_index);
-    correct &= (request->source[current_char_index] == ' ');
-    correct &= (current_char_index < len);
-    if (correct) {
-        request->source[current_char_index] = '\0';
-    }
-    current_char_index++;
-
-
-    // URL
+    skip_string_terminating_with_target(request->source, &current_char_index, len, ' ', &correct);
     request->url = request->source + current_char_index;
-    advance_to_next_whitespace(request->source, &current_char_index);
-    correct &= (request->source[current_char_index] == ' ');
-    correct &= (current_char_index < len);
-    if (correct) {
-        request->source[current_char_index] = '\0';
-    }
-    current_char_index++;
-
-
-    // VERSION
+    skip_string_terminating_with_target(request->source, &current_char_index, len, ' ', &correct);
     request->version = request->source + current_char_index;
-    advance_to_next_carriage_return(request->source, &current_char_index);
-    correct &= (request->source[current_char_index] == '\r');
-    correct &= (current_char_index < len);
-    if (correct) {
-        request->source[current_char_index++] = '\0';
-        correct &= (request->source[current_char_index++] == '\n');
-    }
-
-
-
-    // HEADERS
-    while (
-        (request->source)[current_char_index] != '\r' && 
-        (request->source)[current_char_index] != '\n' && 
-        (request->source)[current_char_index] != '\0' &&
-		(correct &= (request->headers_num < MAX_HEADERS))
-    ){
-        if (current_char_index >= len) {
-            correct = false;
-            break;
-        }
-
-        // HEADER NAME
-        header_names[request->headers_num] = request->source + current_char_index;
-        advance_to_next_targets(request->source, &current_char_index, ":\r\n");
-        correct &= (request->source[current_char_index] == ':');
-        correct &= (current_char_index < len);
-        if(correct){ 
-            request->source[current_char_index++] = '\0';
-        }
-
-        // HEADER VALUE
-        correct &= (request->source[current_char_index++] == ' ');
-        while(current_char_index < len && request->source[current_char_index] == ' '){
-            current_char_index++;
-        }
-        correct &= (request->source[current_char_index] != '\r');
-        correct &= (request->source[current_char_index] != '\n');
-    	correct &= (current_char_index < len);
-		header_values[request->headers_num++] = request->source + current_char_index;
-    
-
-        // HEADER CLOSING
-        advance_to_next_carriage_return(request->source, &current_char_index);
-        correct &= (request->source[current_char_index] == '\r');
-		if(correct){
-            request->source[current_char_index++] = '\0';
-		}
-        correct &= (request->source[current_char_index++] == '\n');
-    } 
-
-
-    correct &= (current_char_index < len);
+    skip_string_terminating_with_target(request->source, &current_char_index, len, '\r', &correct);
     if (correct){
-        request->source[current_char_index - 1] = '\0';
+        correct &= (request->source[current_char_index++] == '\n');
     }
-    correct &= (request->source[current_char_index++] == '\r');
-    correct &= (request->source[current_char_index++] == '\n');
-    
+
+
+    /* HEADERS */ {
+        while (
+            (request->source)[current_char_index] != '\r' && 
+            (request->source)[current_char_index] != '\n' && 
+            (request->source)[current_char_index] != '\0' &&
+            (correct &= (request->headers_num < MAX_HEADERS))
+        ){
+            if (current_char_index >= len) {
+                correct = false;
+                break;
+            }
+            header_names[request->headers_num] = request->source + current_char_index;
+            skip_string_terminating_with_target_safe(request->source, &current_char_index, len, ':', ":\r\n", &correct);
+            skip_character_safe(request->source, &current_char_index, len, ' ', "\r\n", &correct);
+            header_values[request->headers_num++] = request->source + current_char_index;
+            advance_to_next_carriage_return(request->source, &current_char_index);
+            correct &= (request->source[current_char_index] == '\r');
+            if(correct){
+                request->source[current_char_index++] = '\0';
+            }
+            correct &= (request->source[current_char_index++] == '\n');
+        } 
+
+        correct &= (current_char_index < len);
+        if (correct){
+            request->source[current_char_index - 1] = '\0';
+        }
+        correct &= (request->source[current_char_index++] == '\r');
+        correct &= (request->source[current_char_index++] == '\n');
+    }
 
     // PAYLOAD
     request->payload = request->source + current_char_index;
     correct &= (request->payload - request->method) < len;
     
+    // FINALIZE
     correct &= ( strcmp(request->method, "") != 0);
     correct &= ( strcmp(request->version, "") != 0);
     correct &= ( strcmp(request->url, "") != 0);
-
-    // FINALIZE
     if (correct) {
 	    request->header_names = (char**) malloc (sizeof(char*) * request->headers_num);
 	    request->header_values = (char**) malloc (sizeof(char*) * request->headers_num);
